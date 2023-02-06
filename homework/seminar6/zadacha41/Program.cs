dddd30f75e90f1bea65f49efa6656c725289d7d4
Yulia <yuliaperepechina@gmail.com>
2023-02-06 13:36:08 +0300

    add hw6

---
+++ homework/seminar6/zadacha41/Program.cs
@@ -0,0 +1,18 @@
+﻿// ввод с клавиатуры М чисел. посчитать сколько чисел больше 0 ввели
+
+System.Console.WriteLine("Введите количество чисел");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+int sum = 0;
+for (int i = 0; i < m; i++)
+{
+    System.Console.WriteLine("Введите число");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0) sum = sum + 1;
+}
+for (int i = 0; i < m; i++)
+{
+    System.Console.Write(" " + array[i]);
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Количество числе больше 0 равно " + sum);
---
+++ homework/seminar6/zadacha43/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите коэффициент b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = (b1 - b2) / (k2 - k1);
+double y = k1 * x + b1;
+if (k1 == k2 && b1 == b2) System.Console.WriteLine("Прямые совпадают");
+else if (k1 == k2) System.Console.WriteLine("Прямые параллельны");
+else System.Console.WriteLine("Координаты точки пересечения (" + x + " ;" + y + " )");
+
